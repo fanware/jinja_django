8ba227c59006eeea13d9cf3f8c24ac8c42147841
fanware <fanuelamukune@gmail.com>
2024-11-12 18:02:36 +0300

    initial commit

---
+++ jinjaapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from jinjaapp import views
+
+
+urlpatterns=[
+    path('',views.home,name='my_home'),
+    path('about/',views.about,name='my_about'),
+    path('contact/',views.contact,name='my_contact'),
+
+]
